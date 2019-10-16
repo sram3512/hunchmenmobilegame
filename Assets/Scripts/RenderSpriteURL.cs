6455d5c26b584480b51a706ec79e7c1188b5f5f2
Sriram Anupindi <sriramanupindi94@gmail.com>
2019-10-16 15:45:42 -0700

    remote image render trial

---
+++ Assets/Scripts/RenderSpriteURL.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderSpriteURL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer img;
+    private string url="http://spriteserver.sanupindi.com:5000/hello";
+   	IEnumerator Start()
+    {
+        WWW serverUrl = new WWW(url);
+        yield return serverUrl;
+        img.sprite = Sprite.Create(serverUrl.texture,new Rect(0, 0, serverUrl.texture.width, serverUrl.texture.height), new Vector2(0, 0));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
