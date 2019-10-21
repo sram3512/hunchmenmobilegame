581d4b2209bda687953b8e9c0614368696a7361d
Sriram Anupindi <sriramanupindi94@gmail.com>
2019-10-20 22:09:07 -0700

    user level tracking json

--- Assets/Scripts/StaticClass.cs
+++ Assets/Scripts/StaticClass.cs
@@ -5,4 +5,5 @@ public static class StaticClass
    public static string LevelSelection{get; set;}
    public static string ThemeSelection{get; set;}
    public static string KeyBoardInput{get; set;}
+   public static int MaxLevels{get; set;}
 }
